95d278ade46dd4c6f73763ef4aa4d7e104c7fe92
ragulgandhir <ragul.r@hubspire.com>
2022-03-06 21:00:52 +0530

    changes in api

--- src/api/mealApi.js
+++ src/api/mealApi.js
@@ -2,14 +2,6 @@ import axios from 'axios';
 
 const URL = "https://www.themealdb.com/api/json/v1/1"
 
-export const getAllCategories = () => {
-    return axios.get(`${URL}/categories.php`)
-}
-
-export const getMealsByCategory = (category) => {
-    return axios.get(`${URL}/filter.php?c=${category}`)
-}
-
 export const getRandomMeal = () => {
     return axios.get(`${URL}/random.php`)
 }
